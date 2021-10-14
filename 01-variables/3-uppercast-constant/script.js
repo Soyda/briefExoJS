3842a384d0de24a5efd0202f1e31df9a87c4fd6b
Soyda <stevendruesne@gmail.com>
2021-10-14 15:06:21 +0200

    uppercase constant done

--- 01-variables/3-uppercast-constant/script.js
+++ 01-variables/3-uppercast-constant/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// Il est jsute d'utiliser des majuscules
+// pour la constante birthday car c'est une valeur 
+// qui ne changera jamais peu importe ce qu'il peut 
+// se passer.
+
+// La variable age en revanche dépend de birthday
+// et probablement de la date du jour où 
+// le code est exécuté pourra être amené à changer.
