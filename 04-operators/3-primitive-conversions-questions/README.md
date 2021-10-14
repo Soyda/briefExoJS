2a32473fbe45946a2b3ff5c9b0a2b78c13a3e8a9
Soyda <stevendruesne@gmail.com>
2021-10-14 16:41:11 +0200

    primitive conversion done

--- 04-operators/3-primitive-conversions-questions/README.md
+++ 04-operators/3-primitive-conversions-questions/README.md
@@ -7,20 +7,20 @@ importance: 5
 Quels sont les résultats de ces expressions ?
 
 ```js no-beautify
-"" + 1 + 0
-"" - 1 + 0
-true + false
-6 / "3"
-"2" * "3"
-4 + 5 + "px"
-"$" + 4 + 5
-"4" - 2
-"4px" - 2
-"  -9  " + 5
-"  -9  " - 5
-null + 1
-undefined + 1
-" \t \n" - 2
+"" + 1 + 0      // (a)
+"" - 1 + 0      // (b)
+true + false    // (c)
+6 / "3"         // (d)
+"2" * "3"       // (e)
+4 + 5 + "px"    // (f)
+"$" + 4 + 5     // (g)
+"4" - 2         // (h)
+"4px" - 2       // (i)
+"  -9  " + 5    // (j)
+"  -9  " - 5    // (k)
+null + 1        // (l)
+undefined + 1   // (m)
+" \t \n" - 2    // (n)
 ```
 
 Réfléchissez bien, notez et comparez avec la réponse.
--- 04-operators/3-primitive-conversions-questions/script.js
+++ 04-operators/3-primitive-conversions-questions/script.js
@@ -1,4 +1,14 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// (a) "20" car string+number donne un nombre en type string et donc n'effectue pas l'opération mathématique et place les caractères les uns à côté des autres
+// (b) 1 car le - convertit le string en number
+// (c) 1 car true = 1 et false = 0
+// (d) 2 car le / convertit un nombre de string vers number
+// (e) 6 idem que la division (d)
+// (f) "9px" car de gauche à droite on effectue l'opération puis on transforme en string
+// (g) "$45" idem que (a)
+// (h) 2 idem que (b)
+// (i) NaN car les lettres ne peuvent pas être converties en number
+// (j) "  -9  5" idem que (a), les espaces sont des caractères
+// (k) -14 idem que (b) les espaces disparaissent en type number
+// (l) 1 car null vaut 0
+// (m) NaN car l'opération ne s'effectuer
+// (n) -2 car \t et \n sont considérés comme des espaces
