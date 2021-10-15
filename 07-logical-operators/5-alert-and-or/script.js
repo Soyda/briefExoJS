a60f72c3c57199ca85ebf8e63daf2017184a53b1
Soyda <stevendruesne@gmail.com>
2021-10-15 11:02:38 +0200

    alert and or done

--- 07-logical-operators/5-alert-and-or/script.js
+++ 07-logical-operators/5-alert-and-or/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// cela renvoie 3
+// car le OR choisi la première valeur true est le AND puis le §AND 
+// choisit sa dernière valeur car il n'y pas de false, le 3 est donc
+// affiché et cela s'arrête là
