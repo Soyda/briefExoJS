e7951c9097415a8826b56aa536040167d4763dde
Soyda <stevendruesne@gmail.com>
2021-10-15 09:44:29 +0200

    comparison quest done

--- 05-comparison/1-comparison-questions/README.md
+++ 05-comparison/1-comparison-questions/README.md
@@ -7,12 +7,12 @@ importance: 5
 Quel sera le résultat pour les expressions suivantes :
 
 ```js no-beautify
-5 > 4
-"apple" > "pineapple"
-"2" > "12"
-undefined == null
-undefined === null
-null == "\n0\n"
-null === +"\n0\n"
+5 > 4                   // (a)
+"apple" > "pineapple"   // (b)
+"2" > "12"              // (c)
+undefined == null       // (d)
+undefined === null      // (e)
+null == "\n0\n"         // (f)
+null === +"\n0\n"       // (g)
 ```
 
--- 05-comparison/1-comparison-questions/script.js
+++ 05-comparison/1-comparison-questions/script.js
@@ -1,4 +1,7 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// (a) true
+// (b) false car dans la table ASCII p est plus grand que a
+// (c) true car en string le 2 et le 1 sont comparés en premier
+// (d) true car les deux sont égaux, le type n'est pas pris en compte avec le ==
+// (e) false car avec === on prend le type en compte
+// (f) false car un string avec un caractère 0 n'est pas considéré comme nul
+// (g) false car les types sont différents
