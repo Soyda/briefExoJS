05e809fadc7ee2238bb41d8d5bca67f5a7d868e8
Soyda <stevendruesne@gmail.com>
2021-10-15 10:45:43 +0200

    alert or done

--- 07-logical-operators/2-alert-or/script.js
+++ 07-logical-operators/2-alert-or/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// cela donne 1 puis 2
+// car avant d'être évaluée alert(1) est exécutée, une valeur false
+// est ensuite renvoyée ce qui fait qu'ensuite le 2 est évalué
+// qui lui est true donc 2 est affiché cela s'arrête là.
