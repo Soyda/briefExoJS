25fec8da8a396a08031d966886348d084b8ce311
Soyda <stevendruesne@gmail.com>
2021-10-15 11:56:36 +0200

    check login done

--- 07-logical-operators/9-check-login/script.js
+++ 07-logical-operators/9-check-login/script.js
@@ -1,4 +1,17 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let userName = prompt("Who are you ?","");
+
+if ( userName == "Admin" ) {
+      let password = prompt("Password ?","");
+
+      if (password == "TheMaster") {
+            alert("Welcome");
+      } else if (password == '' || password == null) {
+            alert("Canceled");
+      } else {
+            alert("Wrong password");
+      }
+} else if (userName == '' || userName == null) {
+      alert("Canceled");
+} else {
+      alert("I don't know you");
+}
