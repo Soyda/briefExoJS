8e644f56825c245cb591021bc5a0e480c3a660be
Soyda <stevendruesne@gmail.com>
2021-10-15 10:55:26 +0200

    alert and done

--- 07-logical-operators/4-alert-and/script.js
+++ 07-logical-operators/4-alert-and/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// cela renvoie 1 puis undefined
+// de la même manière qu'avec le OR avant, alert(1) est exécuté puis 
+// renvoie une valeur undefined qui false et est donc affiché car il 
+// est le premier false 
