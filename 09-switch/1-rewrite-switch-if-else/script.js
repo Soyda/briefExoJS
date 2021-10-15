135cd6e84eb7cc2c639b658f0cbe53cbde8b78c0
Soyda <stevendruesne@gmail.com>
2021-10-15 12:34:58 +0200

    rewrite switch if else done

--- 09-switch/1-rewrite-switch-if-else/script.js
+++ 09-switch/1-rewrite-switch-if-else/script.js
@@ -1,4 +1,9 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let browser = prompt("What is your browser?",'');
+
+if ( browser == 'Edge') {
+      alert("You've dot the Edge!");
+} else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+      alert("Okay we support these browsers too");
+} else {
+      alert( 'We hope that this page looks ok!' );
+}
