ff1cd910f0a79238bcaa46af483b0e10305f467c
Soyda <stevendruesne@gmail.com>
2021-10-15 10:23:39 +0200

    rewrite if question done

--- 06-ifelse/5-rewrite-if-question/script.js
+++ 06-ifelse/5-rewrite-if-question/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+
+let a = +prompt('Enter number a:','');
+let b = +prompt('Enter number b:','');
+
+let result = ( a + b ) < 4 ? 'Below' : 'Over';
+
+alert(result);
+
