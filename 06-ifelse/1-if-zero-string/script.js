efa85857c0f7de75519f33e845122abda274683d
Soyda <stevendruesne@gmail.com>
2021-10-15 09:48:03 +0200

    if zero string done

--- 06-ifelse/1-if-zero-string/script.js
+++ 06-ifelse/1-if-zero-string/script.js
@@ -1,4 +1,1 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// Oui alert sera affiché car "0" est considéré comme true
