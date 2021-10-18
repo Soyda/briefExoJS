bea7880e6d93a206ecc51a48b9b040feb5b94ffe
Soyda <stevendruesne@gmail.com>
2021-10-18 11:29:39 +0200

    which value while done

--- 12-while-for/2-which-value-while/script.js
+++ 12-while-for/2-which-value-while/script.js
@@ -1,4 +1,9 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// la première boucle affiche 1, 2, 3, 4,
+// la seconde  boucle affiche 1, 2, 3, 4, 5
+
+let i = 0;
+while (++i < 5) alert( i ); // i est incrémenté avant la vérification de la condition
+
+
+let j = 0;
+while (j++ < 5) alert( j ); // j est comparé avant l'incrémetation et l'alert infiche la valeur incrémenté
