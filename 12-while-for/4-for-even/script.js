94f2fcb62a11df549a615f5289bd2ad3bcbd8669
Soyda <stevendruesne@gmail.com>
2021-10-18 12:22:04 +0200

    for even done

--- 12-while-for/4-for-even/script.js
+++ 12-while-for/4-for-even/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+
+for (let i = 0; i <= 10; i++) {
+    if ( i % 2 == 0 ) alert(i);
+}
