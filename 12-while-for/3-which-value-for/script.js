c73a820fce898d26e264d3b99b9f942b803b4077
Soyda <stevendruesne@gmail.com>
2021-10-18 11:51:48 +0200

    which value for done

--- 12-while-for/3-which-value-for/script.js
+++ 12-while-for/3-which-value-for/script.js
@@ -1,4 +1,9 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+
+// les valeurs affichées sont les mêmes pour les deux versions : 0, 1, 2, 3, 4
+// la boucle s'exécute avant la comparaison donc que l'incrémentation soit en préfixe ou postfixe ne change rien
+
+
+for (let i = 0; i < 5; i++) alert( i ); 
+
+
+for (let i = 0; i < 5; ++i) alert( i );
